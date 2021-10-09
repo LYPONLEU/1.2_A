73e0119398e06ee43d58504cb2a839b832ca01b0
LYPONLEU <ponleu.l190149@sis.hust.edu.vn>
2021-10-09 15:41:15 +0700

    1.2_A

---
+++ app/src/main/java/LY/PONLEU/MainActivity.java
@@ -0,0 +1,14 @@
+package LY.PONLEU;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
